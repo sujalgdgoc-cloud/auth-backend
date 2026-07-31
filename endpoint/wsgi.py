637af6edae28e7119bab3fa71c295b03dcbd6577
Sujal <Sujalgdgoc@gmail.com>
2026-07-31 11:38:10 +0530

    updated wsgi.py for deployement

--- endpoint/wsgi.py
+++ endpoint/wsgi.py
@@ -9,6 +9,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'endpoint.settings')
