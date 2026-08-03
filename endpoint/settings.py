1df2cc0111b284e9c804d23eaccaa4917bb0992b
Sujal <Sujalgdgoc@gmail.com>
2026-08-03 10:53:03 +0530

    settings updated v3

--- endpoint/settings.py
+++ endpoint/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
-    "corsheadersrest_framework"
+    "corsheadersrest_framework",
     "accounts",
 ]
 
